5f798b3225a256f0a4cab03b21cde96d8794ba5e
denisson <denisson.graca3@gmail.com>
2023-02-09 21:10:25 -0300

    first

---
+++ videos.js
@@ -0,0 +1,30 @@
+export default [
+    {
+        src: "./img/01.mp4",
+        title: "Cogumelos life",
+      },
+      {
+        src: "./img/02.mp4",
+        title: "Animals from Australia",
+      },
+      {
+        src: "./img/03.mp4",
+        title: "dance guy",
+      },
+      {
+        src: "./img/04.mp4",
+        title: "animation",
+      },
+      {
+        src: "./img/05.mp4",
+        title: "flower",
+      },
+      {
+        src: "./img/06.mp4",
+        title: "animal in watter",
+      },
+      {
+        src: "./img/07.mp4",
+        title: "Beautifull place",
+      },
+]
